4b3e246d3b105c29602a3beea92300d2c3189d1f
Christoph Praschl <christoph.praschl@fh-hagenberg.at>
2021-09-29 10:42:40 +0200

    Cleaning up project

--- scripts/validate.bat
+++
@@ -1,8 +0,0 @@
-set VALIDATOR_INPUTS=inputs
-set VALIDATOR_OUTPUTS=reports
-set VALIDATOR_HOME=.
-set VALIDATOR_CONFIG=config
-chcp 65001
-java -Xms4g -cp target/ocl_validator-3.0-jar-with-all-dependencies.jar ocl.OCLEvaluator
-PAUSE
-
---
+++ src/main/java/ocl/OclVerificationTest.java
@@ -0,0 +1,64 @@
+package ocl;
+
+import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.ocl.*;
+import org.eclipse.ocl.ecore.*;
+import org.eclipse.ocl.ecore.OCL;
+import org.eclipse.ocl.ecore.impl.VariableImpl;
+import org.eclipse.ocl.ecore.internal.UMLReflectionImpl;
+import org.eclipse.ocl.pivot.model.OCLstdlib;
+import java.util.List;
+
+public class OclVerificationTest {
+    public static void main(String[] args) throws ParserException {
+        // Create package
+        EPackage pack = EcoreFactory.eINSTANCE.createEPackage();
+        pack.setName("MyGreatPackage");
+
+        // Create classes
+        EClass personClass = EcoreFactory.eINSTANCE.createEClass();
+        personClass.setName("Person");
+        pack.getEClassifiers().add(personClass);
+
+        // Create attributes
+        EAttribute ageAttribute = EcoreFactory.eINSTANCE.createEAttribute();
+        ageAttribute.setName("age");
+        ageAttribute.setEType(EcorePackage.eINSTANCE.getEInt());
+        personClass.getEAttributes().add(ageAttribute);
+
+        // Add the created classes to the UML reflection, so it can be used
+        UMLReflectionImpl.INSTANCE.asOCLType(personClass);
+
+        // Setup OCL
+        OCLstdlib.install();
+        OCL ocl = OCL.newInstance();
+        Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> environment = ocl.getEnvironment();
+        environment.getContextPackage();
+        // hack the variable impl into the environment with one type of the package to implicitly set the contextPackage as required for ocl
+        // This is the most hacky shit ever, but it works. Who the fuck hides important setters like setContextPackage which is required for OCL.
+        // We will never, ever talk about this bullshit. It took > 2 days to get this run...
+        org.eclipse.ocl.expressions.Variable<EClassifier, EParameter> variable = new VariableImpl() {
+            @Override
+            public EClassifier getType() {
+                return personClass;
+            }
+        };
+        environment.setSelfVariable(variable);
+
+        // Create object instance, which should be evaluated
+        EFactory factory = EcoreFactory.eINSTANCE.createEFactory();
+        factory.setEPackage(pack);
+        EObject person = factory.create(personClass);
+        person.eSet(ageAttribute, 5);
+
+        // Try OCL Constraint
+        OCLInput oclInput = new OCLInput("context Person inv: self.age >= 5");
+        List<Constraint> parse = ocl.parse(oclInput);
+        for(Constraint constraint : parse){
+            boolean check = ocl.check(person, constraint);
+            System.out.println(check);
+        }
+    }
+}
